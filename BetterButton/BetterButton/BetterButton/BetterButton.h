3a6207257c00bae9fca361890efe147d8442f7ca
Brandon Danis <bdani093@uottawa.ca>
2017-10-16 21:21:19 -0400

    Init framework

---
+++ BetterButton/BetterButton/BetterButton/BetterButton.h
@@ -0,0 +1,19 @@
+//
+//  BetterButton.h
+//  BetterButton
+//
+//  Created by Brandon Danis on 2017-10-16.
+//  Copyright © 2017 Brandon Danis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BetterButton.
+FOUNDATION_EXPORT double BetterButtonVersionNumber;
+
+//! Project version string for BetterButton.
+FOUNDATION_EXPORT const unsigned char BetterButtonVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BetterButton/PublicHeader.h>
+
+
