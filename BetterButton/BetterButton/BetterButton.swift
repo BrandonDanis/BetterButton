b6c56c5ee694fa8549e161b49b6c1cafd804fd51
Brandon Danis <bdani093@uottawa.ca>
2017-10-16 21:53:40 -0400

    Created BetterButton class and added DS_STORE to gitignore

---
+++ BetterButton/BetterButton/BetterButton.swift
@@ -0,0 +1,21 @@
+//
+//  BetterButton.swift
+//  BetterButton
+//
+//  Created by Brandon Danis on 2017-10-16.
+//  Copyright © 2017 Brandon Danis. All rights reserved.
+//
+
+import UIKit
+
+public class BetterButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
